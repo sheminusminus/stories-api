63fa6e8b46b15024d8ee6aa1193b4e241df70822
Emily Kolar <emkolar@gmail.com>
2020-01-20 21:31:23 -0800

    add json parsing, cors, env variables

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
+require('dotenv').config();
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express();
-const port = 3000;
+const port = 3005;
 
-app.get('/ck-token', (req, res) => {
+app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const getToken = (userId, userName) => {
+  const key = process.env.CK_SECRET_KEY;
+  const envId = process.env.CK_ENV_ID;
+  return jwt.sign({
+    iat: Math.floor(Date.now() / 1000),
+    iss: envId,
+    user: {
+      id: userId,
+      name: userName,
+    },
+    auth: {
+      collaboration: {
+        '*': {
+          role: 'writer',
+        },
+      },
+    },
+  }, key, { algorithm: 'HS256' });
+};
+
+app.post('/api/ck-token', (req, res) => {
+  const { userId, userName } = req.body;
+  const token = getToken(userId, userName);
+  res.set('Content-Type', 'application/json');
+  res.send(token);
 });
 
 app.get('/', (req, res) => res.send('Hello World!'));
